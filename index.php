98ebd304f24c7aff3abefefe2d94d8d6e478327b
pasindu745 <pasindu745@gmail.com>
2015-12-10 21:16:49 +0530

    index file added

---
+++ index.php
@@ -0,0 +1,71 @@
+<html>
+	<head>
+		<title>index</title>
+		<?php include 'includes/head.php'; ?><!--css files-->
+	</head>
+	
+	<body>
+		<?php include 'includes/navbar.php';?><!--navbarphp-->
+		
+		<?php include 'includes/slider.php';?><!--slider-->
+
+
+		<div class="container"><!--This is a container-->
+
+			<div class="row"><!--This is row 1-->
+				<div class="col-md-12"><!--column 1-->
+					<h2 align="center">Welcome to lms bla bla.... etc...lol !!</h2>
+					<p>Wrie anything that you want.You can write what ever the fk You want !!.Lets put something that touch users heart.
+					Wrie anything that you want.You can write what ever the fk You want !!.Lets put something that touch users heart.
+					Wrie anything that you want.You can write what ever the fk You want !!.Lets put something that touch users heart.
+					</p>
+				</div>
+				
+			</div><br><br>
+
+
+			<div class="row">
+				<div class="col-sm-4">
+					<div class="panel panel-default text-center" >
+						<div class="panel-body">
+							<span class="glyphicon glyphicon-ok"></span>
+							<H4>panel</H4>
+							<p>TYPE SOMETHING TYPE SOMETHING</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-4">
+					<div class="panel panel-default text-center" >
+						<div class="panel-body">
+							<span class="glyphicon glyphicon-ok"></span>
+							<H4>panel</H4>
+							<p>TYPE SOMETHING TYPE SOMETHING</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-4">
+					<div class="panel panel-default text-center" >
+						<div class="panel-body">
+							<span class="glyphicon glyphicon-ok"></span>
+							<H4>panel</H4>
+							<p>TYPE SOMETHING TYPE SOMETHING</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!--This is a container1-->
+		<br>
+		
+		<?php include 'includes/footer.php';?>
+			
+		
+		
+	
+	
+	
+	<script src="js/jquery-1.11.3.min.js"></script><!--Javascript file-->
+	<script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
